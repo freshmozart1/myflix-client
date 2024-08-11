b8ad81927a5bb3db0cee07ad4029871282c1d759
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-08-11 16:17:05 +0200

    I added bootstrap styling to the LoginView component.

--- src/components/LoginView/login-view.jsx
+++ src/components/LoginView/login-view.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import PropTypes from 'prop-types';
+import './login-view.scss';
 
 export const LoginView = ({ onLoggedIn }) => {
     const [username, setUsername] = useState(''),
         [password, setPassword] = useState(''),
+        [errorMessage, setErrorMessage] = useState(''),
         handleSubmit = (event) => {
             event.preventDefault();
             const data = {
@@ -14,26 +16,33 @@ export const LoginView = ({ onLoggedIn }) => {
                 method: 'POST'
             }).then(res => {
                 if (res.ok) {
-                    res.json().then(res => {
-                        localStorage.setItem('user', JSON.stringify(res.user));
-                        localStorage.setItem('token', res.token);
-                        onLoggedIn(res.user, res.token);
+                    res.json().then(res_ok => {
+                        localStorage.setItem('user', JSON.stringify(res_ok.user));
+                        localStorage.setItem('token', res_ok.token);
+                        onLoggedIn(res_ok.user, res_ok.token);
                     })
                     .catch(e => console.error('Couldn\'t convert response to JSON: ' +e ));
                 } else {
-                    alert('Login failed');
+                    res.json().then(res_error => {
+                        setErrorMessage(res_error.message);
+                    });
                 }
             })
             .catch(e => console.error('Error logging in: ' + e));
         };
-
+    
     return (
         <form onSubmit={handleSubmit}>
-            <label htmlFor="login_username">Username</label>
-            <input type="text" id="login_username" value={username} onChange={(e) => {setUsername(e.target.value)}} required />
-            <label htmlFor="login_password">Password</label>
-            <input type="password" id="login_password" value={password} onChange={(e) => {setPassword(e.target.value)}} required />
-            <button type="submit">Submit</button>
+            <div className="form-group">
+                <label htmlFor="login_username">Username</label>
+                <input type="text" className="form-control" id="login_username" value={username} onChange={(e) => { setUsername(e.target.value) }} minLength={process.env.USERNAME_LENGTH} required />
+            </div>
+            <div className="form-group">
+                <label htmlFor="login_password">Password</label>
+                <input type="password" className="form-control" id="login_password" value={password} onChange={(e) => { setPassword(e.target.value) }} minLength={process.env.PASSWORD_LENGTH} required />
+            </div>
+            <small className="error">{errorMessage}</small><br />
+            <button type="submit" className="btn btn-primary">Submit</button>
         </form>
     );
 };
