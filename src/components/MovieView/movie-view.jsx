c50adc5c26a81ab1f9ba4ae2c8367dc24fe10227
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-08-13 17:17:17 +0200

    I removed unused PropTypes from the MovieView component.

--- src/components/MovieView/movie-view.jsx
+++ src/components/MovieView/movie-view.jsx
@@ -1,5 +1,3 @@
-import PropTypes from 'prop-types';
-
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
@@ -56,29 +54,4 @@ export const MovieView = () => {
             <Spinner animation="border" role="status" aria-label='Loading movie' />
         </Container>
     );
-};
-
-MovieView.propTypes = {
-    movie: PropTypes.exact({
-        _id: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        genre: PropTypes.exact({
-            _id: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            description: PropTypes.string.isRequired,
-            __v: PropTypes.number.isRequired
-        }),
-        director: PropTypes.exact({
-            _id: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            birthday: PropTypes.string.isRequired,
-            deathday: PropTypes.string,
-            biography: PropTypes.string.isRequired,
-            __v: PropTypes.number.isRequired
-        }),
-        imagePath: PropTypes.string,
-        __v: PropTypes.number.isRequired
-    }).isRequired,
-    onBackClick: PropTypes.func.isRequired
 };
