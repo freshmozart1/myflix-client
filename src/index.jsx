f2ef9ccbbf8f9aaf35148c1980959b1a24038d65
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-08-13 15:32:17 +0200

    added a react router.

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,31 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/MainView/main-view";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { StrictMode } from 'react';
+import { MovieList } from "./components/MovieList/movie-list";
+
 import './index.scss';
 
-const MyFlixApplication = () => {
+/*const MyFlixApplication = () => {
     return (
         <MainView />
     );
 };
 
-createRoot(document.querySelector('#root')).render(<MyFlixApplication />);
+createRoot(document.querySelector('#root')).render(<MyFlixApplication />);*/
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainView />,
+        children: [
+            { index: true, element: <MovieList /> }
+        ]
+    }
+]);
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
