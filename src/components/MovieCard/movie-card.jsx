55961afe22271c5d8538e5d30e3b9a7484758d93
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-08-21 19:57:41 +0200

    A click on the favourite button now updates the database.

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -2,8 +2,39 @@ import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 import './movie-card.scss';
 import { RippleButton } from '../RippleButton/ripple-button';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../AuthProvider/auth-provider';
 
 export const MovieCard = ({ movie, onClick }) => {
+    const { user, token, setUser } = useContext(AuthContext);
+    const [isFavourite, setIsFavourite] = useState(() => {
+        if (user.favourites) return user.favourites.includes(movie._id);
+        return false;
+    });
+    const patchFavourites = () => {
+        if (isFavourite) {
+            user.favourites = user.favourites.filter(favourite => favourite !== movie._id);
+            if (user.favourites.length === 0) user.favourites = null;
+        } else {
+            if (!user.favourites) user.favourites = [];
+            user.favourites.push(movie._id);
+        }
+        fetch(process.env.HEROKU + '/users/' + user.username, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({ favourites: user.favourites })
+        }).then(respone => {
+            if (respone.ok) {
+                setUser(user);
+                setIsFavourite(!isFavourite);
+            } else {
+                console.error('Failed to patch favourites: ' + respone.status);
+            }
+        });
+    }
     return (
         <Card className="z-0 position-relative" style={{cursor: 'pointer'}} onClick={onClick}>
             <Card.Img className="z-0 position-relative" variant="top" src={'.' + movie.imagePath} />
@@ -14,8 +45,9 @@ export const MovieCard = ({ movie, onClick }) => {
                 </Card.Title>
                 <RippleButton className="favouriteButton" onClick={(e) => {
                     e.stopPropagation();
+                    patchFavourites();
                 }} rippleColor={(() => getComputedStyle(document.documentElement).getPropertyValue('--bs-yellow').trim())()}>
-                    <i className="bi bi-star"></i>
+                    <i className={`bi bi-star${isFavourite ? '-fill' : ''}`}></i>
                 </RippleButton>
                 <Card.Text>{movie.description}</Card.Text>
             </Card.Body>
