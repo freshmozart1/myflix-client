ab9ba09c1ad65d9028fedea91574ac8524dc9004
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-08-30 11:44:19 +0200

    Add "new movie" card Fixes #8

--- src/components/MovieList/movie-list.jsx
+++ src/components/MovieList/movie-list.jsx
@@ -2,8 +2,11 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Row, Col, Container, Spinner } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-
+import Card from 'react-bootstrap/Card';
 import { MovieCard } from '../MovieCard/movie-card';
+import { RippleButton } from '../RippleButton/ripple-button';
+
+import './movie-list.scss';
 
 export const MovieList = (props) => {
     const navigate = useNavigate();
@@ -26,13 +29,26 @@ export const MovieList = (props) => {
             <Spinner animation="border" role="status" aria-label='Loading movie' />
         </Container>
     ) : (
-        <Row>
-            {movies.map((movie) => (
-                <Col key={movie._id} xs={12} md={6} lg={4} xl={3} className='g-md-4 d-flex align-items-stretch'>
-                    <MovieCard movie={movie} onClick={() => navigate(`/movies/${movie.title}`)} />
-                </Col>
-            ))}
-        </Row>
+            <Row>
+                {
+                    props.movies ? '' : <Col xs="12" md="6" lg="4" xl="3" className="g-md-4 d-flex align-items-stretch">
+                        <Card style={{position: 'relative', width: '100%', borderColor: getComputedStyle(document.documentElement).getPropertyValue('--bs-primary').trim() }}>
+                            <Card.Body style={{position: 'relative'}}>
+                                <RippleButton className="addNewMovieButton" onClick={() => {}} rippleColor={(() => getComputedStyle(document.documentElement).getPropertyValue('--bs-primary').trim())()}>
+                                    <i className="bi bi-plus"></i>
+                                </RippleButton>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                }
+                {
+                    movies.map((movie) => (
+                        <Col xs="12" md="6" lg="4" xl="3" className="g-md-4 d-flex align-items-stretch" key={movie._id}>
+                            <MovieCard movie={movie} onClick={() => navigate(`/movies/${movie.title}`)} />
+                        </Col>
+                    ))
+                }
+            </Row>
     );
 };
 
