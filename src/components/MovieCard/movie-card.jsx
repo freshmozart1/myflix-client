8fa65c7d217cb26124e3034f327429de268970ae
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-09-18 18:50:31 +0200

    Renamed aws env vars.

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -23,8 +23,8 @@ export const MovieCard = ({ movie, onClick }) => {
                 const s3 = new S3Client({
                     region: process.env.S3_REGION,
                     credentials: {
-                        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+                        accessKeyId: process.env.AWS_ID,
+                        secretAccessKey: process.env.AWS_SECRET
                     }
                 });
                 const thumbnailResponse = await s3.send(new GetObjectCommand({
--- src/components/MovieView/movie-view.jsx
+++ src/components/MovieView/movie-view.jsx
@@ -32,8 +32,8 @@ export const MovieView = () => {
                 const s3 = new S3Client({
                     region: process.env.S3_REGION,
                     credentials: {
-                        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+                        accessKeyId: process.env.AWS_ID,
+                        secretAccessKey: process.env.AWS_SECRET
                     }
                 });
                 const posterResponse = await s3.send(new GetObjectCommand({
