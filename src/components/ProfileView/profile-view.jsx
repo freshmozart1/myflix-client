f57d5ce1e059084cfecbde40da354d03cae93ee7
freshmozart1 <78731534+freshmozart1@users.noreply.github.com>
2024-09-19 11:11:26 +0200

    Removed TODO

--- src/components/ProfileView/profile-view.jsx
+++ src/components/ProfileView/profile-view.jsx
@@ -7,7 +7,7 @@ import { Col, Row } from 'react-bootstrap';
 import { MovieList } from '../MovieList/movie-list';
 import './profile-view.scss';
 
-export const ProfileView = () => { //TODO: #19 This component should use the REST APIs favourites route
+export const ProfileView = () => {
     const { user, token, setUser, setToken } = useContext(AuthContext);
     if (!user || !token) {
         window.location.href = '/';
